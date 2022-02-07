799af913220ac38bd93d468529a4205503b62667
gyamfidennist <79916056+gyamfidennist@users.noreply.github.com>
2022-02-07 11:49:19 +0000

    update to email validation. phone number validation function added

--- public/assets/js/functions/genericFunctions.js
+++ public/assets/js/functions/genericFunctions.js
@@ -46,10 +46,16 @@ function somethingWentWrongHandler() {
     }, 3000);
 }
 
-function validateEmail($email) {
+function validateEmail(email) {
     let emailRegx =
         /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return emailRegx.test($email);
+    return emailRegx.test(email);
+}
+
+function validatePhone(phoneNumber) {
+    let phoneRegex =
+        /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/;
+    return phoneRegex.test(phoneNumber);
 }
 
 function currencyConvertor(
