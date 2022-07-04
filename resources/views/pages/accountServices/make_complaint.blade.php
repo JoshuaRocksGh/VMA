f3c258d86a01eb5760138ea7402d2fe8c70f09c4
gyamfidennist <79916056+gyamfidennist@users.noreply.github.com>
2022-07-04 13:42:20 +0000

    moved redundant files to trash

--- resources/views/pages/accountServices/make_complaint.blade.php
+++ resources/views/pages/accountServices/make_complaint.blade.php
@@ -7,76 +7,72 @@ $basePath = 'account services';
 $currentPath = 'complaint';
 @endphp
 @include('snippets.pageHeader')
-<div class="pt-3">
-    <div class="row px-2 px-md-3">
-        <div class="col-lg-6 site-card" id="request_form_div">
-            <form action="#" class="select_beneficiary mx-auto mt-3" style="max-width: 650px;" id="payment_details_form"
-                autocomplete="off" aria-autocomplete="none">
-                @csrf
+<div class="w-100 dashboard site-card" id="request_form_div">
+    <div class="w-100  dashboard-body p-3 mx-auto my-3 ">
+        <form action="#" class="select_beneficiary pt-4 mx-auto" id="payment_details_form" style="max-width: 650px;"
+            autocomplete="off" aria-autocomplete="none">
+            @csrf
 
-                <div class="form-group  mb-4">
-                    <label class=" text-primary">Select Account</label>
-                    <select class="form-control accounts-select " id="from_account" required>
-                        <option selected disabled value=""> -- Select Account --</option>
-                        @include('snippets.accounts')
-                    </select>
+            <div class="form-group  mb-4">
+                <label class=" text-primary">Select Account</label>
+                <select class="form-control accounts-select " id="from_account" required>
+                    <option selected disabled value=""> -- Select Account --</option>
+                    @include('snippets.accounts')
+                </select>
 
-                </div>
-                <div class="form-group mb-4">
-                    <label class=" text-primary"> Select Service Type</label>
-                    <select name="" id="service_type" class="form-control">
-                        <option disabled value="">---- select a service type ----</option>
-                    </select>
-                </div>
-                <div class="form-group mb-4">
-                    <label class=" text-primary" for="description"> Description</label>
-                    <textarea name="" id="description" class="form-control "></textarea>
-                </div>
-                <div class="form-group text-right ">
-                    <button type="button"
-                        class="btn btn-primary btn-rounded waves-effect waves-light disappear-after-success"
-                        id="proceed_button">
-                        Proceed
-                    </button>
-                </div>
-            </form>
+            </div>
+            <div class="form-group mb-4">
+                <label class=" text-primary"> Select Service Type</label>
+                <select name="" id="service_type" class="form-control">
+                    <option disabled value="">---- select a service type ----</option>
+                </select>
+            </div>
+            <div class="form-group mb-4">
+                <label class=" text-primary" for="description"> Description</label>
+                <textarea name="" id="description" class="form-control "></textarea>
+            </div>
+            <div class="form-group text-right ">
+                <button type="button"
+                    class="btn btn-primary btn-rounded waves-effect waves-light disappear-after-success"
+                    id="proceed_button">
+                    Proceed
+                </button>
+            </div>
+        </form>
+    </div>
 
-        </div>
-        <div class="col-lg-6 d-none d-lg-block">
-            <div class="card">
-                <div class="card-body">
-                    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel"
-                        style="min-height: 120px; max-height: auto;">
-                        <div class="carousel-inner" role="listbox">
-                            <div class="carousel-item active">
-                                <img class="d-block img-fluid" style="min-height: 100%"
-                                    src="{{ asset('assets/images/rokel/sim_korpor_5.jpeg') }}" alt="First slide">
-                            </div>
-                            <div class="carousel-item">
-                                <img class="d-block img-fluid" style="height: auto;"
-                                    src="{{ asset('assets/images/rokel/sim_korpor_6.jpeg') }}" alt="Second slide">
-                            </div>
-                            <div class="carousel-item">
-                                <img class="d-block img-fluid" style="min-height"
-                                    src="{{ asset('assets/images/rokel/sim_korpor_7.jpeg') }}" alt="Third slide">
-                            </div>
-                        </div>
-                        <a class="carousel-control-prev" href="#carouselExampleControls" role="button"
-                            data-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                        <a class="carousel-control-next" href="#carouselExampleControls" role="button"
-                            data-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Next</span>
-                        </a>
+</div>
+{{-- <div class="col-lg-6 d-none d-lg-block">
+    <div class="card">
+        <div class="card-body">
+            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel"
+                style="min-height: 120px; max-height: auto;">
+                <div class="carousel-inner" role="listbox">
+                    <div class="carousel-item active">
+                        <img class="d-block img-fluid" style="min-height: 100%"
+                            src="{{ asset('assets/images/rokel/sim_korpor_5.jpeg') }}" alt="First slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block img-fluid" style="height: auto;"
+                            src="{{ asset('assets/images/rokel/sim_korpor_6.jpeg') }}" alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block img-fluid" style="min-height"
+                            src="{{ asset('assets/images/rokel/sim_korpor_7.jpeg') }}" alt="Third slide">
                     </div>
                 </div>
+                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
             </div>
         </div>
     </div>
-</div>
+</div> --}}
 @endsection
 
 @section('scripts')
