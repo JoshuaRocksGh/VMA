baf4b6f31f4286427512dec755b788a2e88bbd0c
gyamfidennist <79916056+gyamfidennist@users.noreply.github.com>
2022-07-19 13:32:16 +0000

    payment beneficiaries

--- resources/views/pages/transfer/beneficiary_list.blade.php
+++ resources/views/pages/transfer/beneficiary_list.blade.php
@@ -32,12 +32,16 @@
         color: white !important;
     }
 
+    .current-type {
+        max-width: 250px !important;
+    }
+
     .beneficiary-text {
-        color: rgb(216, 216, 216)
+        color: rgb(245, 239, 239)
     }
 
     .display-card {
-        height: 4rem;
+        height: 3rem;
         background-color: rgba(255, 255, 255, 0.5);
         backdrop-filter: blur(5px);
         box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
@@ -45,8 +49,10 @@
         display: flex !important;
         justify-content: center;
         align-items: center;
-        width: 150px;
+        max-width: 200px;
+        width: 100%;
         cursor: pointer;
+        transition: all 0.3s ease-in-out;
 
     }
 
@@ -76,32 +82,33 @@ $currentPath = "Transfer Beneficiary";
     <div class="dashboard site-card ">
         <div class="row dashboard-body p-2">
             <div class="col-md-12">
-                <h2 class="font-17 text-left font-weight-bold text-capitalize mb-3 text-primary">select Beneficiary type
+                <h2 class="font-14 text-left font-weight-bold text-capitalize mb-3 text-primary">select Beneficiary type
                 </h2>
-                <div class="row mb-4 justify-content-center mx-auto" style="max-width: 750px;">
-                    <div class="col-md-3 mb-2 mx-2 mx-lg-3  beneficiary-type current-type display-card bg-same-bank"
+                <div class="d-md-flex  mb-4 justify-content-center mx-auto" style="max-width: 750px;">
+                    <div class=" mb-2 mx-2 mx-lg-3 mx-auto beneficiary-type current-type display-card bg-same-bank"
                         data-bene-type="SAB" data-title="Same Bank" id=''>
                         <span class="box-circle"></span>
-                        <span class="mt-1 beneficiary-text" id=''>Same Bank</span>
+                        <span class="mt-1 beneficiary-text font-14" id=''>Same Bank</span>
                     </div>
 
-                    <div class="col-md-3 mb-2 mx-2 mx-lg-3 beneficiary-type display-card  bg-other-bank"
+                    <div class=" mb-2 mx-2 mx-auto mx-lg-3 beneficiary-type display-card  bg-other-bank"
                         data-bene-type="OTB" data-title="Other Bank" id=''>
                         <span class="box-circle"></span>
-                        <span class="mt-1 beneficiary-text" id=''>Other Local Bank</span>
+                        <span class="mt-1 beneficiary-text font-14" id=''>Other Local Bank</span>
                     </div>
-                    <div class="col-md-3 mb-2 mx-2 mx-lg-3 beneficiary-type display-card  bg-international-bank"
+                    <div class=" mb-2 mx-2 mx-lg-3 mx-auto beneficiary-type display-card  bg-international-bank"
                         data-bene-type="INTB" data-title="International Bank" id=''>
                         <span class="box-circle"></span>
-                        <span class="mt-1 beneficiary-text" id=''>International Bank</span>
+                        <span class="mt-1 beneficiary-text font-14" id=''>International Bank</span>
                     </div>
                 </div>
                 <hr>
                 <div class="row justify-content-center">
                     <h3 class="font-15 text-capitalize text-center mx-3 font-weight-bold align-self-center my-auto"><i
-                            class="font-18 text-info mx-2 fa fa-user-friends"></i><span id="beneficiary_type_title">Same
+                            class="font-18 text-info mx-2  fa fa-user-friends"></i><span id="beneficiary_type_title"
+                            class=" font-14">Same
                             Bank
-                        </span>Beneficiaries</h3>
+                        </span><span class=" font-14">Beneficiaries</span></h3>
                     <button type="button" class="btn px-3 btn-sm font-14 font-weight-bold btn-info btn-rounded"
                         id="add_beneficiary"><i class="pr-2 fa fa-user-plus"></i>Add</button>
                 </div>
