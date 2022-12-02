956ccce6c759a39deb4b383d8504be44b54125c3
josh.tetteh01 <josh.tetteh01@gmail.com>
2022-12-02 11:30:31 +0000

    ..

--- resources/views/pages/dashboard/right_aside.blade.php
+++ resources/views/pages/dashboard/right_aside.blade.php
@@ -57,7 +57,7 @@
         <div class="site-card-body">
             <div class="mb-2 font-13"> <i class="mr-1 fas fa-user text-danger"></i><span class="font-weight-bold">Name
                     :
-                </span><span>Sierra Leone Commerical Bank Ltd</span> </div>
+                </span><span>Sierra Leone Commerical Bank</span> </div>
             <div class="mb-2 font-13"> <i class="mr-1 fas fa-phone text-danger"></i><span class="font-weight-bold">Phone
                     :</span> <span>(+232) - 22 -225264</span> </div>
             <div class="mb-2 font-13"> <i class="mr-1 fas fa-envelope text-danger"></i><span
