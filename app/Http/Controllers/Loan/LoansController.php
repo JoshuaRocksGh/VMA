33fc82ad7d7cd670b744b90983ac3af684a41241
josh.tetteh01 <josh.tetteh01@gmail.com>
2022-12-09 11:08:05 +0000

    ..

--- app/Http/Controllers/Loan/LoansController.php
+++ app/Http/Controllers/Loan/LoansController.php
@@ -172,17 +172,16 @@ class LoansController extends Controller
         // });
 
         // $response = Http::get(env('API_BASE_URL') . "/account/accountFD/$customerNumber");
-        try{
-            $response = Http::retry(20, 100)->post(env('API_BASE_URL') . "loans/getLoans", $data);
+        try {
+            // $response = Http::retry(20, 100)->post(env('API_BASE_URL') . "loans/getLoans", $data);
+            $response = Http::post(env('API_BASE_URL') . "loans/getLoans", $data);
 
-        $result = new ApiBaseResponse();
-        return $result->api_response($response);
-        }catch (\Exception $error) {
+            $result = new ApiBaseResponse();
+            return $result->api_response($response);
+        } catch (\Exception $error) {
             // Log::alert($error);
             return $base_response->api_response('500', $error,  NULL); // return API BASERESPONSE
         }
-
-
     }
 
     public function get_Loan_products()
