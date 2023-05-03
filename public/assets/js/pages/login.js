32135f1e2b985c74545200be6dd4ab76da66f8b4
josh.tetteh01 <josh.tetteh01@gmail.com>
2023-05-03 15:42:12 +0000

    ..

--- public/assets/js/pages/login.js
+++ public/assets/js/pages/login.js
@@ -9,7 +9,7 @@ function login(email, password) {
         url: "login-api",
         datatype: "application/json",
         data: {
-            _token: $('meta[name="csrf-token"]').attr('content'),
+            _token: $('meta[name="csrf-token"]').attr("content"),
             deviceOS: deviceOS,
             deviceType: deviceType,
             deviceID: deviceID,
@@ -348,28 +348,25 @@ function submitSecurityQuestion(userData) {
     });
 }
 
- // Set the amount of time (in milliseconds) before the user is considered inactive
- var inactivityTime = 180000; // 5 mins
+// Set the amount of time (in milliseconds) before the user is considered inactive
+var inactivityTime = 180000; // 5 mins
 
- // Set a timer that will trigger after the specified amount of time
- var timeoutId = setTimeout(function () {
-     // This code will execute when the user has been inactive for the specified amount of time
+// Set a timer that will trigger after the specified amount of time
+var timeoutId = setTimeout(function () {
+    // This code will execute when the user has been inactive for the specified amount of time
     //  console.log("User is inactive");
-    location.reload()
- }, inactivityTime);
-
+    location.reload();
+}, inactivityTime);
 
 // Add event listeners to detect when the user interacts with the page
-$(document).on("mousemove keydown", function() {
+$(document).on("mousemove keydown", function () {
     // If the user interacts with the page, reset the timer
     clearTimeout(timeoutId);
-    timeoutId = setTimeout(function() {
+    timeoutId = setTimeout(function () {
         // console.log("User is inactive.......");
-        location.reload()
+        location.reload();
     }, inactivityTime);
 });
-
-
 
 $(document).ready(function () {
     const userData = new Object();
@@ -377,8 +374,6 @@ $(document).ready(function () {
     //     var csrfToken = generateCsrfToken(); // replace this with your own function to generate the CSRF token
     //     document.getElementById('csrf-token').value = csrfToken;
     //   }, 60000); // update the token every 60 seconds (or change this to your desired interval)
-
-
 
     $("#submit").on("click", function (e) {
         e.preventDefault();
